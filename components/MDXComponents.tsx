d02d2bc1c4c2e11d353892ad798a4732955040af
devarsh98 <devarsh@devenvs.io>
2025-10-28 18:46:40 +0530

    modify home, project and about page

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,10 +1,13 @@
 import TOCInline from 'pliny/ui/TOCInline'
 import Pre from 'pliny/ui/Pre'
-import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
 import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
+
+// Provide a no-op BlogNewsletterForm so any MDX files that reference it
+// won't break. This intentionally renders nothing to remove the UI.
+const BlogNewsletterForm = () => null
 
 export const components: MDXComponents = {
   Image,
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,34 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Deploy 3-Tier App on EC2 using Docker Networking Concept.',
+    description: `Are you wondering how things actually work in the backend of things? Take a deep dive by clicking on this hands-on begineer project.`,
+    imgSrc: '/static/images/docker-nw-img.png',
+    href: 'https://devarshshah.hashnode.dev/deploying-3-tier-application-on-aws-ec2-using-docker-networking-concept',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'O11y with Grafana Tempo and OpenTelemetry',
+    description: `Are you looking for some exciting Observability intermidiate project? This the is the best hands-on project, if you want to know about how the monitoring works. Go through it.`,
+    imgSrc: '/static/images/o11y-img.png',
+    href: 'https://devarshshah.hashnode.dev/o11y-with-grafana-tempo-and-opentelemetry',
+  },
+  {
+    title: 'Deploying React App on K8s',
+    description: `Are you looking for some exciting Observability intermidiate project? This the is the best hands-on project, if you want to know about how the monitoring works. Go through it.`,
+    imgSrc: '/static/images/k8s.png',
+    href: 'https://devarshshah.hashnode.dev/deployment-on-k8s',
+  },
+  {
+    title: 'Getting Started with ArgoCD',
+    description: `Are you wondering how getting started with ArgoCD looks like? Here's the blog which I wrote for my future self. I have picked up this thumbnail image from devtron's blog`,
+    imgSrc: '/static/images/argocd.png',
+    href: 'https://devarshshah.hashnode.dev/getting-started-with-argocd',
+  },
+  {
+    title: 'CI/CD Project using GitHub Actions and ArgoCD',
+    description: `Gone through the getting started guide of ArgoCD? Let us build one project then.`,
+    imgSrc: '/static/images/argocicd.png',
+    href: 'https://devarshshah.hashnode.dev/cicd-project',
   },
 ]
 
